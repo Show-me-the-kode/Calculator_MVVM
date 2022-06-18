bce40b03ae4c52ab8b37d2451c0a95cc9f8837c7
s9hn <tnghqn@naver.com>
2022-06-18 17:58:15 +0900

    [Feat] 메인계산기 뷰 작업
    1. 기록뷰 세팅?

--- app/src/main/java/sehun/org/calculator_mvvm/presentation/CalculatorActivity.kt
+++ app/src/main/java/sehun/org/calculator_mvvm/presentation/CalculatorActivity.kt
@@ -1,7 +1,7 @@
 package sehun.org.calculator_mvvm.presentation
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import sehun.org.calculator_mvvm.R
 import sehun.org.calculator_mvvm.databinding.ActivityCalculatorBinding
@@ -9,19 +9,40 @@ import sehun.org.calculator_mvvm.presentation.record.RecordFragment
 
 class CalculatorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalculatorBinding
+    private var visibility = INVISIBLE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initFragment()
+        changeVisibility()
     }
 
     private fun initFragment() {
         val recordFragment = RecordFragment()
 
-        supportFragmentManager.beginTransaction().add(R.id.fcv_main_torecord, recordFragment)
+        supportFragmentManager.beginTransaction().add(R.id.fcv_calculator_torecord, recordFragment)
             .commit()
-        Log.d("ㅇㅇㅇ", "ㅇㅇㅇㅇ")
+    }
+
+    private fun changeVisibility() {
+        binding.ivCalculatorTorecord.setOnClickListener {
+            when (visibility) {
+                INVISIBLE -> {
+                    binding.fcvCalculatorTorecord.visibility = View.VISIBLE
+                    visibility = VISIBLE
+                }
+                VISIBLE -> {
+                    binding.fcvCalculatorTorecord.visibility = View.GONE
+                    visibility = INVISIBLE
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val INVISIBLE = 8
+        const val VISIBLE = 0
     }
 }
